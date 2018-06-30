c0e1d511f42f74c863dba962f219dbb24de53ebd
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-06-30 15:44:01 +0100

    Issue #1: using grid-stride loop

--- examples/nvidia_example_add.cu
+++ examples/nvidia_example_add.cu
@@ -10,10 +10,10 @@
 __global__
 void add(int n, float *x, float *y)
 {
-  int index = threadIdx.x;
-  int stride = blockDim.x;
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
   for (int i = index; i < n; i += stride)
-      y[i] = x[i] + y[i];
+    y[i] = x[i] + y[i];
 }
 
 int main(void)
@@ -32,7 +32,9 @@ int main(void)
   }
 
   // Run kernel on 1M elements on the GPU
-  add<<<1, 256>>>(N, x, y);
+  int blockSize = 256;
+  int numBlocks = (N + blockSize - 1) / blockSize;
+  add<<<numBlocks, blockSize>>>(N, x, y);
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
