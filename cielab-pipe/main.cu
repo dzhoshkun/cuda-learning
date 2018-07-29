300dfdfcbef22e1cb5759343e0017ebd2d3b00ad
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-07-29 12:11:44 +0100

    Issue #11: added OpenCV image loader and saver

--- cielab-pipe/main.cu
+++ cielab-pipe/main.cu
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <opencv2/highgui/highgui.hpp>
 
 int main(int argc, char *argv[])
 {
@@ -6,6 +7,20 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         printf("Usage: %s <path-to-image>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    // load image
+    cv::Mat orig_img, proc_img;
+    orig_img = cv::imread(argv[1]);
+
+    // TODO: delete this, testing only
+    proc_img = orig_img;
+
+    // save processed image
+    if (not cv::imwrite("./cielab-pipe-processed.jpg", proc_img))
+    {
+        printf("Could not write processed image\n");
         return EXIT_FAILURE;
     }
 
