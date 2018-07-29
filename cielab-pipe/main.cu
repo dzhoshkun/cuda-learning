c716b8c299428855d64143f06aff3afd369c098b
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-07-29 17:35:59 +0100

    Issue #11: added I420-to-BGR conversion on device

--- cielab-pipe/main.cu
+++ cielab-pipe/main.cu
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <nppi.h>
 
 int main(int argc, char *argv[])
 {
@@ -14,20 +15,44 @@ int main(int argc, char *argv[])
     // load image
     cv::Mat orig_img;
     orig_img = cv::imread(argv[1]);
+    int n_rows = orig_img.rows, n_cols = orig_img.cols;
 
     // convert image to I420
     cv::Mat orig_img_i420;
     cv::cvtColor(orig_img, orig_img_i420, cv::COLOR_BGRA2YUV_YV12);
 
     // prepare data pointers
-    void *h_data = (void *) orig_img_i420.data, *d_data;
+    Npp8u *h_data = (Npp8u *) orig_img_i420.data, *d_data;
 
     // allocate device memory
     int count = orig_img_i420.total() * orig_img_i420.elemSize();
-    cudaMalloc(&d_data, count);
+    cudaMalloc((void **) &d_data, count);
 
     // copy data to device
     cudaMemcpy(d_data, h_data, count, cudaMemcpyHostToDevice);
+
+    // allocate memory for BGR data on device
+    Npp8u *d_data_bgr;
+    cudaMalloc(&d_data_bgr, 3 * count * sizeof(Npp8u));
+
+    // convert I420 to BGR on device
+    {
+        Npp8u *pSrc[3], *pDst = d_data_bgr;
+        pSrc[0] = (Npp8u *) d_data;
+        pSrc[1] = (Npp8u *) &d_data[n_rows * n_cols]; // TODO: is address op used correctly
+        // TODO: assert offset integer
+        pSrc[2] = (Npp8u *) &d_data[(int)(n_rows * n_cols * 1.25)]; // TODO: is address op used correctly
+        int rSrcStep[3], nDstStep = 3 * n_cols;
+        rSrcStep[0] = n_cols;
+        // TODO: assert offset integer
+        rSrcStep[1] = (int)(n_cols * 0.25);
+        // TODO: assert offset integer
+        rSrcStep[2] = (int)(n_cols * 0.25);
+        NppiSize oSizeROI;
+        oSizeROI.width = n_cols;
+        oSizeROI.height = n_rows; // TODO: check row vs. col order!
+        nppiYUV420ToBGR_8u_P3C3R(pSrc, rSrcStep, pDst, nDstStep, oSizeROI);
+    }
 
     // TODO: delete this, testing only
     cv::Mat proc_img_i420;
@@ -37,6 +62,7 @@ int main(int argc, char *argv[])
     cudaMemcpy(h_data, d_data, count, cudaMemcpyDeviceToHost);
 
     // free device memory
+    cudaFree(d_data_bgr);
     cudaFree(d_data);
 
     // save processed image
