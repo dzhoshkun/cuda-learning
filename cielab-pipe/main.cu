d050a0f0646a728802f78a6d72301b3667852dfc
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-07-29 11:37:50 +0100

    Issue #11: added sanity check for image parameter

--- cielab-pipe/main.cu
+++ cielab-pipe/main.cu
@@ -1,5 +1,15 @@
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
+    // sanity check
+    if (argc != 2)
+    {
+        printf("Usage: %s <path-to-image>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    // all good
     return EXIT_SUCCESS;
 }
 
