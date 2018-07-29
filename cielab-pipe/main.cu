9423344e68226881dad7bda4cd0434156278252a
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-07-29 13:19:31 +0100

    Issue #11: added H-to-D and -back memory copy

--- cielab-pipe/main.cu
+++ cielab-pipe/main.cu
@@ -19,9 +19,25 @@ int main(int argc, char *argv[])
     cv::Mat orig_img_i420;
     cv::cvtColor(orig_img, orig_img_i420, cv::COLOR_BGRA2YUV_YV12);
 
+    // prepare data pointers
+    void *h_data = (void *) orig_img_i420.data, *d_data;
+
+    // allocate device memory
+    int count = orig_img_i420.total() * orig_img_i420.elemSize();
+    cudaMalloc(&d_data, count);
+
+    // copy data to device
+    cudaMemcpy(d_data, h_data, count, cudaMemcpyHostToDevice);
+
     // TODO: delete this, testing only
     cv::Mat proc_img_i420;
     proc_img_i420 = orig_img_i420;
+
+    // copy data back to host
+    cudaMemcpy(h_data, d_data, count, cudaMemcpyDeviceToHost);
+
+    // free device memory
+    cudaFree(d_data);
 
     // save processed image
     cv::Mat proc_img;
