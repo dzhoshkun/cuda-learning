9dd60d8c1bd0cdfea030314147b3fb29695d0d34
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-08-03 12:31:39 +0100

    Issue #12: reading in sample FullHD image

--- cupy-argsort/do-cupy-argsort.py
+++ cupy-argsort/do-cupy-argsort.py
@@ -1,5 +1,8 @@
+import skimage.io
 import cupy
 
 if __name__ == '__main__':
-    print(cupy.argsort)
+    filename = '/home/dzhoshkun/data/sample-images/1920x1080.jpg'
+    img_orig = skimage.io.imread(filename)
+    print(img_orig.shape)
 
