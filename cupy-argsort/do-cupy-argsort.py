13436b4247a885221424c5233b8a11fafa760ba2
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-08-03 12:37:46 +0100

    Issue #12: copying a channel of Lab image to device

--- cupy-argsort/do-cupy-argsort.py
+++ cupy-argsort/do-cupy-argsort.py
@@ -6,5 +6,6 @@ if __name__ == '__main__':
     filename = '/home/dzhoshkun/data/sample-images/1920x1080.jpg'
     img_orig = skimage.io.imread(filename)
     img_lab = skimage.color.rgb2lab(img_orig)
-    print(img_lab.shape)
+    d_img_a = cupy.array(img_lab[:, :, 1])
+    print(d_img_a.shape)
 
