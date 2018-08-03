38c46dd3675217f65408ae25cb3d9c3a54f0d499
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-08-03 12:32:56 +0100

    Issue #12: converting image to Lab color space

--- cupy-argsort/do-cupy-argsort.py
+++ cupy-argsort/do-cupy-argsort.py
@@ -1,8 +1,10 @@
 import skimage.io
+import skimage.color
 import cupy
 
 if __name__ == '__main__':
     filename = '/home/dzhoshkun/data/sample-images/1920x1080.jpg'
     img_orig = skimage.io.imread(filename)
-    print(img_orig.shape)
+    img_lab = skimage.color.rgb2lab(img_orig)
+    print(img_lab.shape)
 
