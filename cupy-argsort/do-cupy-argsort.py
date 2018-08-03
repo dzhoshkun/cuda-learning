c75ff989c8a03e59863591e8b88882b8d0514e93
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-08-03 15:28:00 +0100

    Issue #12: profiling cupy.argsort call

--- cupy-argsort/do-cupy-argsort.py
+++ cupy-argsort/do-cupy-argsort.py
@@ -1,12 +1,22 @@
+import cProfile
+import StringIO
+import pstats
 import skimage.io
 import skimage.color
 import cupy
 
 if __name__ == '__main__':
+    pr = cProfile.Profile()
     filename = '/home/dzhoshkun/data/sample-images/1920x1080.jpg'
     img_orig = skimage.io.imread(filename)
     img_lab = skimage.color.rgb2lab(img_orig)
     d_img_a = cupy.array(img_lab[:, :, 1])
+    pr.enable()
     d_img_a_idx = cupy.argsort(d_img_a)
-    print(d_img_a_idx.shape)
+    pr.disable()
+    s = StringIO.StringIO()
+    sortby = 'tottime'
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    ps.print_stats()
+    print(s.getvalue())
 
