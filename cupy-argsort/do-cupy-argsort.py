b61d73da3cd5ac1f7a38c7e19a914ed73b56fd3f
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-08-05 10:41:23 +0100

    Issue #12: flattening image before calling argsort

--- cupy-argsort/do-cupy-argsort.py
+++ cupy-argsort/do-cupy-argsort.py
@@ -13,12 +13,15 @@ if __name__ == '__main__':
     img_lab = skimage.color.rgb2lab(img_orig)
     d_img_a = cupy.array(img_lab[:, :, 1])
     pr.enable()
-    img_a_idx = numpy.argsort(img_lab[:, :, 1])
-    d_img_a_idx = cupy.argsort(d_img_a)
+    img_a_idx = numpy.argsort(img_lab[:, :, 1].flatten())
+    d_img_a_idx = cupy.argsort(d_img_a.flatten())
     pr.disable()
     s = StringIO.StringIO()
     sortby = 'tottime'
     ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
     ps.print_stats()
     print(s.getvalue())
+    print(cupy.max(d_img_a_idx),
+          img_orig.shape[0] * img_orig.shape[1])
+    print(d_img_a_idx.shape)
 
